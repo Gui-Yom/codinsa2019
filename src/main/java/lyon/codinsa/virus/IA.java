c6c2a7ff86d463c2d87ba06666e3474ce1381d60
AlexisLeConte <alexis.leconte@outlook.fr>
2019-03-31 00:12:40 +0100

    Fix AI

--- src/main/java/lyon/codinsa/virus/IA.java
+++ src/main/java/lyon/codinsa/virus/IA.java
@@ -7,101 +7,51 @@ import lyon.codinsa.virus.network.Action;
 import lyon.codinsa.virus.network.Board;
 import lyon.codinsa.virus.network.Game;
 import lyon.codinsa.virus.network.Graph;
-import lyon.codinsa.virus.network.Node;
 import lyon.codinsa.virus.network.Visible;
 import lyon.codinsa.virus.network.WaitResponse;
 
-class IAThread implements Runnable {
-    private Game game;
-    
-    public IAThread(Game game) {
-        this.game = game;
-    }
-
-    @Override
-    public void run() {
-        System.out.println(game.getToken());
-        do {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {}
-        } while (game.gameWait().wait.equals("true"));
-        System.out.println(game.getToken());
-        
-        
-        WaitResponse response;
-        VirusAI ai = new BasicAI(game.getId()); // Change AI here
-        do {
-            Board board = game.getBoard();
-            Visible visible = game.getVisible();
-            
-            Graph graph = new Graph(board, visible);
-            System.out.println(game.getToken() + " : " + graph);
-            LinkedList<Action> actions = ai.play(graph);
-            game.play(actions);
-            response = game.endTurn();
-            do {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {}
-            } while (game.gameWait().wait.equals("true"));
-        } while(response.partyEnd.equals("success"));
-    }
-}
-
 public class IA {
+    
+    public static String generateName() {
+        String name = "";
+        for(int i=0; i<16; ++i) {
+            name += (char)('a' + (int)(Math.random() * 26));
+        }
+        return name;
+    }
 
     public static void main(String[] args) throws InterruptedException {
 
-        Game game1 = new Game("INSA", "http://127.0.0.1:8080");
-        game1.reset();
-        game1.join();
+        Game game = new Game(generateName(), "http://127.0.0.1:8080");
+        // game.reset();
+        game.join();
+        System.out.println(game.getToken());
         
-        Game game2 = new Game("LYON", "http://127.0.0.1:8080");
-        game2.join();
-        game2.startGame();
-        /*.out.println("erhreh");
-        do {
+        //do {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(10000);
             } catch (InterruptedException e) {}
-        } while (game1.gameWait().wait.equals("true"));*/
-        System.out.println(game1.getToken());
-        
-        Game game = game1;
+        //} while (!game.gameWait().wait.equals("true"));
+        game.startGame();
         
         WaitResponse response;
         VirusAI ai = new BasicAI(game.getId()); // Change AI here
         do {
             Board board = game.getBoard();
             Visible visible = game.getVisible();
-            
             Graph graph = new Graph(board, visible);
-            System.out.println(game.getToken() + " : " + graph);
             LinkedList<Action> actions = ai.play(graph);
             game.play(actions);
             response = game.endTurn();
-
             System.out.println(game.getToken() + " : " + graph);
             System.out.println("P1: " + game.getId());
-            
-            
-            
-            Board board2 = game2.getBoard();
-            Visible visible2 = game2.getVisible();
-            
-            Graph graph2 = new Graph(board2, visible2);
-            System.out.println(game2.getToken() + " : " + graph2);
-            LinkedList<Action> actions2 = ai.play(graph2);
-            game2.play(actions2);
-            response = game2.endTurn();
-            System.out.println("P2: " + game2.getId());
-            /*do {
+            game.gameWait();
+            //do {
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(3000);
                 } catch (InterruptedException e) {}
-            } while (game.gameWait().wait.equals("true"));*/
-        } while(response.partyEnd.equals("success"));
+            //} while (game.gameWait().wait.equals("true"));
+        } while(!response.partyEnd.equals("success"));
     }
 
 }
