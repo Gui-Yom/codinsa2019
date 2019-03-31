282681cbae463ab0fc0ba3fdbb4a793ddf0f0f4e
AlexisLeConte <alexis.leconte@outlook.fr>
2019-03-31 01:37:45 +0100

    Merge

--- src/main/java/lyon/codinsa/virus/ai/BasicAI.java
+++ src/main/java/lyon/codinsa/virus/ai/BasicAI.java
@@ -1,5 +1,7 @@
 package lyon.codinsa.virus.ai;
 
+import java.util.Collections;
+import java.util.Comparator;
 import lyon.codinsa.virus.network.Action;
 import lyon.codinsa.virus.network.Graph;
 import lyon.codinsa.virus.network.Link;
@@ -20,11 +22,22 @@ public class BasicAI extends VirusAI {
         LinkedList<Action> attacks = new LinkedList<>();
         LinkedList<Node> myNodes = stateUpdate.getPlayerNodes(id);
         for (Node n : myNodes) {
-            for (Link link : n.getNeighbors()) {
-                if (n.getQtCode() > 0) {
-                    Integer qt = Math.min(n.getQtCode(), link.debit);
-                    if (n.getQtCode() - qt > 0)
-                        attacks.add(new Action(n.getId(), link.id, qt));
+            LinkedList<Link> links = new LinkedList(n.getNeighbors());
+            Collections.sort(links, new Comparator<Link>() {
+                @Override
+                public int compare(Link o1, Link o2) {
+                    Integer value1 = stateUpdate.getNode(o1.id).getQtCode();
+                    Integer value2 = stateUpdate.getNode(o2.id).getQtCode();
+                    return value1 - value2;
+                }
+            });
+            for (Link link : links) {
+                if (stateUpdate.getNode(link.id).getOwner().equals(id)) {
+                    continue;
+                }
+                if (n.getQtCode() > 1) {
+                    Integer qt = Math.min(n.getQtCode()-1, link.debit);
+                    attacks.add(new Action(n.getId(), link.id, qt));
                 }
             }
         }
