e1ff17296897f7ee8002f3489da42a66e43044a5
LimeiloN <memorial.limelion@gmail.com>
2019-03-31 01:44:04 +0100

    wip

--- src/main/java/lyon/codinsa/virus/network/Action.java
+++ src/main/java/lyon/codinsa/virus/network/Action.java
@@ -1,6 +1,6 @@
 package lyon.codinsa.virus.network;
 
-public class Action {
+public class Action implements Comparable<Action> {
 
     public int owner;
     public int from;
@@ -14,6 +14,7 @@ public class Action {
         this.to = to;
         this.qtCode = qtCode;
     }
+
     public Action(int from, int to, int qtCode, Integer potential) {
 
         this.from = from;
@@ -21,24 +22,10 @@ public class Action {
         this.qtCode = qtCode;
         this.potential = potential;
     }
+
     public int compareTo(Action action) {
+
         return potential.compareTo(action.potential);
     }
-    /*
-    public static List<Action> batchCreate(int... values) {
-
-        if (values.length % 3 != 0) {
-            System.err.println("[Action::batchCreate] Incorrect params / Should be modulo 3");
-            return null;
-        }
-
-        int n = values.length / 3;
-        ArrayList<Action> actions = new ArrayList<>(n);
-        for (int i = 0; i < n; i++) {
-            actions.add(new Action(values[i * n], values[i * n + 1], values[i * n + 2]));
-        }
-        return actions;
-    }
-    */
 
 }
