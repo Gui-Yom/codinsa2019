5c2809aa9e76a38933f71a101650940de528ea80
cheah96 <c-stanley96@hotmail.com>
2019-03-31 10:31:44 +0200

    update advanceAI

--- src/main/java/lyon/codinsa/virus/ai/AdvancedAI.java
+++ src/main/java/lyon/codinsa/virus/ai/AdvancedAI.java
@@ -7,9 +7,7 @@ import lyon.codinsa.virus.network.Node;
 
 import java.util.*;
 
-public class AdvancedAI extends VirusAI {
-<<<<<<< HEAD
-	
+public class AdvancedAI extends VirusAI {	
 	public AdvancedAI(Integer playerId) {
 		super(playerId);
 	}
@@ -124,105 +122,4 @@ public class AdvancedAI extends VirusAI {
 		}
 		return res;
 	}
-=======
-
-    public AdvancedAI(Integer playerId) {
-
-        super(playerId);
-    }
-
-
-    @Override
-    public List<Action> play(Graph stateUpdate) {
-
-        List<Action> attacks = new ArrayList<>();
-        ArrayList<Node> borders = getBorders(stateUpdate);
-        for (Node n : borders) {
-            attacks.addAll(attack(stateUpdate, n, n.getQtCode() - 1));
-        }
-        attacks.addAll(getTransfers(borders, stateUpdate));
-        return attacks;
-    }
-
-    public LinkedList<Action> getTransfers(ArrayList<Node> borders, Graph stateUpdate) {
-
-        LinkedList<Action> possibleActions = new LinkedList<>();
-        HashMap<Node, Link> distance = new HashMap<Node, Link>();
-        for (Node node : borders) {
-            BFS(distance, node, stateUpdate);
-        }
-        for (Node node : stateUpdate.getPlayerNodes(this.id)) {
-            if (!borders.contains(node)) {
-                possibleActions.add(new Action(node.getId(), distance.get(node).id, node.getQtCode() - 1));
-            }
-        }
-        return possibleActions;
-    }
-
-    public void BFS(HashMap<Node, Link> distance, Node node, Graph stateUpdate) {
-
-        LinkedList<Node> toVisit = new LinkedList<Node>();
-        HashMap<Node, Integer> currentDistance = new HashMap<Node, Integer>();
-        toVisit.add(node);
-        currentDistance.put(node, 0);
-        HashSet<Node> visited = new HashSet<Node>();
-        while (!toVisit.isEmpty()) {
-            Node src = toVisit.get(0);
-            for (Link l : src.getNeighbors()) {
-                Node n = stateUpdate.getNode(l.id);
-                if (distance.get(n) == null || (!visited.contains(n) && distance.get(n).debit > currentDistance.get(src) + 1)) {
-                    toVisit.add(n);
-                    visited.add(n);
-                    distance.put(n, new Link(src.getId(), currentDistance.get(src) + 1));
-                    currentDistance.put(n, currentDistance.get(src) + 1);
-                }
-            }
-            toVisit.remove(0);
-        }
-    }
-
-    public ArrayList<Node> getBorders(Graph stateUpdate) {
-
-        ArrayList<Node> borders = new ArrayList<Node>();
-        for (Node node : stateUpdate.getPlayerNodes(this.id)) {
-            if (node.isBorder(stateUpdate)) {
-                borders.add(node);
-            }
-        }
-        return borders;
-    }
-
-
-    public LinkedList<Action> attack(Graph state, Node source, Integer attackCode) {
-
-        LinkedList<Action> possibleActions = new LinkedList<>();
-        LinkedList<Action> res = new LinkedList<>();
-        for (Link link : source.getNeighbors()) {
-            Node n = state.getNode(link.id);
-            if (!n.getOwner().equals(id)) {
-                if (n.getIsServer() && n.getQtCode() <= attackCode)
-                    possibleActions.add(new Action(source.getId(), n.getId(), Math.min(link.debit, attackCode), (double) n.getProduction() / 0.001));
-                else
-                    possibleActions.add(new Action(source.getId(), n.getId(), Math.min(link.debit, n.getQtCode() + 20), (double) n.getProduction() / (n.getQtCode() + 0.001)));
-            }
-        }
-        Collections.sort(possibleActions, Collections.reverseOrder());
-        int remainingCode = attackCode;
-        for (Action a : possibleActions) {
-            if (a.qtCode <= remainingCode) {
-                res.add(a);
-                remainingCode -= a.qtCode;
-            } else if (a == possibleActions.getLast() && a.qtCode - 20 <= remainingCode) {
-                a.qtCode = remainingCode;
-                res.add(a);
-            } else if (a.qtCode - 19 <= remainingCode) {
-                a.qtCode = a.qtCode - 19;
-                res.add(a);
-            } else {
-                break;
-            }
-        }
-        return res;
-    }
->>>>>>> 152be3f05599fc0bc939fbb72ef1c5f4c2a87d35
 }
