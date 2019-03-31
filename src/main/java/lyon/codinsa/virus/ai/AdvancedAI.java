90be351bce88185b50eae0909b1318b75410d953
jaxalo <ismail.jattioui1@gmail.com>
2019-03-31 01:03:02 +0100

    wip

---
+++ src/main/java/lyon/codinsa/virus/ai/AdvancedAI.java
@@ -0,0 +1,23 @@
+package lyon.codinsa.virus.ai;
+
+import java.util.LinkedList;
+
+import lyon.codinsa.virus.network.Action;
+import lyon.codinsa.virus.network.Graph;
+
+public class AdvancedAI extends VirusAI {
+	
+	public AdvancedAI(Integer playerId) {
+		super(playerId);
+	}
+	
+
+	@Override
+	public LinkedList<Action> play(Graph stateUpdate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
