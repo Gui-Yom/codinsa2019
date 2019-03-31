cdd2e7abe56298703e975dbcd1d05f6f0a466b1d
gnomino <sylvain.vaure@insa-lyon.fr>
2019-03-31 01:22:39 +0100

    wip

--- src/main/java/lyon/codinsa/virus/ai/AdvancedAI.java
+++ src/main/java/lyon/codinsa/virus/ai/AdvancedAI.java
@@ -1,9 +1,12 @@
 package lyon.codinsa.virus.ai;
 
+import java.util.Collections;
 import java.util.LinkedList;
 
 import lyon.codinsa.virus.network.Action;
 import lyon.codinsa.virus.network.Graph;
+import lyon.codinsa.virus.network.Link;
+import lyon.codinsa.virus.network.Node;
 
 public class AdvancedAI extends VirusAI {
 	
@@ -19,5 +22,28 @@ public class AdvancedAI extends VirusAI {
 	}
 	
 	
-
+	public LinkedList<Action> attack(Graph state, Node source, Integer attackCode) {
+		LinkedList<Action> possibleActions = new LinkedList<>();
+		for(Link link : source.getNeighbors()) {
+			Node n = state.getNode(link.id);
+			if (!n.getOwner().equals(id)) {
+				possibleActions.add(new Action(source.getId(), n.getId(), Math.min(link.debit, attackCode), n.getProduction() / n.getQtCode()));
+			}
+		}
+		Collections.sort(possibleActions, Collections.reverseOrder());
+		int remainingCode = attackCode;
+		LinkedList<Action> res = new LinkedList<>();
+		for(Action a : possibleActions) {
+			if(a.qtCode <= remainingCode) {
+				res.add(a);
+				remainingCode -= a.qtCode;
+			}
+			else {
+				a.qtCode = remainingCode;
+				res.add(a);
+				break;
+			}
+		}
+		return res;
+	}
 }
